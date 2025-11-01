5edf20fbd185630e25c25f6ccbfab3493745111a
Matthew Stevens <mstevens@llst.ca>
2025-11-01 16:29:50 -0700

    now you are a penguin

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,16 +14,29 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private float _lowJumpMultiplier = 2f;
     [SerializeField] private float _gravity = -25f;
     [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _animationTransitionTime = 0.2f;
+    [SerializeField] private float _maxSqrVelocityForAnimation = 5f;
 
+    private Coroutine _animationCoroutine;
+    
     private bool _isOnGround => 
         Physics.Raycast(transform.position + Vector3.up, Vector3.down, _groundCheckDistance + 1f, _groundLayer);
 
     private bool _hasJustLanded = false;
-    
+
+    private void Awake()
+    {
+        // the free asset I'm using has this coded to running. Should write a wrapper
+        _animator.SetFloat("State",1f);
+    }
+
     void FixedUpdate()
     {
         HandleMovement();
         HandleJump();
+        Debug.Log(_rb.velocity.sqrMagnitude);
+        LerpToAnimationValue(_rb.velocity.sqrMagnitude / _maxSqrVelocityForAnimation);
     }
 
     private void HandleMovement()
@@ -46,6 +60,39 @@ public class PlayerInput : MonoBehaviour
         {
             _rb.AddTorque(Vector3.up * _torqueAmount * Time.deltaTime);
         }
+    }
+
+    private void LerpToAnimationValue(float target)
+    {
+        if (_animationCoroutine != null)
+        {
+            StopCoroutine(_animationCoroutine);
+            _animationCoroutine = null;
+        }
+        
+        _animationCoroutine = StartCoroutine(AnimationTransitionCoroutine(target));
+    }
+
+    private IEnumerator AnimationTransitionCoroutine(float target)
+    {
+        target = Mathf.Clamp01(target);
+        float startValue = _animator.GetFloat("Vert");
+        float elapsedTime = 0f;
+
+        while (elapsedTime < _animationTransitionTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float progress = elapsedTime / _animationTransitionTime;
+            
+            float current = Mathf.Lerp(startValue, target, progress);
+            _animator.SetFloat("Vert", current);
+            
+            yield return null;
+        }
+        
+        // at the end, make sure we made it exactly
+        _animator.SetFloat("Vert", target);
+        _animationCoroutine = null;
     }
 
     private void HandleJump()
